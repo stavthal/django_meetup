e235c0d40c1913b02f46a6cd87aa4046033fd0c1
Stavros Thalassinos <stavros8003@gmail.com>
2024-03-10 17:14:20 +0200

    added new model

--- meetups/models.py
+++ meetups/models.py
@@ -2,10 +2,29 @@ from django.db import models
 from django.utils.text import slugify
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=300)
+    
+    def __str__(self):
+        return f'{self.name} ({self.address})'
+    
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=60)
+    
+    def __str__(self):
+        return self.email
+    
 class Meetup(models.Model): # extends the base class of Model
     title = models.CharField(max_length=200)
-    location = models.CharField(max_length=200)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
     slug = models.SlugField(unique=True, db_index=True)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='images')
+    participants = models.ManyToManyField(Participant, blank=True, null=True)
     
+    def __str__ (self):
+        return f'{self.title} - {self.slug}'
+    
+
