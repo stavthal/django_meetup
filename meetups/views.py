25344e78a6773cd154870dd84271e2bc6f5b05dc
Stavros Thalassinos <stavros8003@gmail.com>
2024-03-10 03:02:07 +0200

    latest changes

--- meetups/views.py
+++ meetups/views.py
@@ -3,4 +3,25 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the meetups index.")
+    # return HttpResponse("Hello, world. You're at the meetups index.")
+    # Render the index.html template
+    meetups = [
+        { 'title': 'A First Meetup', 'location': 'New York', 'slug': 'a-first-meetup' },
+        { 'title': 'A Second Meetup', 'location': 'Paris', 'slug': 'a-second-meetup' },
+        { 'title': 'A Third Meetup', 'location': 'London', 'slug': 'a-third-meetup'}
+    ]
+    return render(request, 'meetups/index.html', {
+        'show_meetups': True,
+        'meetups': meetups
+    })
+    
+def meetup_details(request, meetup_slug):
+    selected_meetup = {
+        'title': 'A First Meetup', 
+        'description': 'This is the first meetup!',
+        'location': 'New York',
+        'image': 'https://images.unsplash.com/photo-1527682497263-6d2e6f24b588'
+    }
+    return render(request, 'meetups/meetup-details.html', {
+        'meetup': selected_meetup
+    })
