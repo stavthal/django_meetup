1902291a59a8a56d080b621b7110c940bd0755a3
Stavros Thalassinos <stavros8003@gmail.com>
2024-03-10 14:00:04 +0200

    another set of migrations

--- meetups/models.py
+++ meetups/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class Meetup(models.Model): # extends the base class of Model
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+    location = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, db_index=True)
     description = models.TextField()
     
