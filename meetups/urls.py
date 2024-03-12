e38ecd2a37e3073a34b78a0f79219dd1e49de57e
Stavros Thalassinos <stavros8003@gmail.com>
2024-03-12 02:13:29 +0200

    registration form redirection

--- meetups/urls.py
+++ meetups/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('meetups/', views.index), # our-domain.com/meetups
+    path('meetups/success', views.confirm_registration, name='confirm_registration'),
     path('meetups/<slug:meetup_slug>', views.meetup_details, name="meetup-details") # our-domain.com/meetups/a-first-meetup
 ]
--- meetups/views.py
+++ meetups/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import Meetup
+from .forms import RegistrationForm
 
 # Create your views here.
 def index(request):
@@ -15,11 +16,28 @@ def index(request):
 def meetup_details(request, meetup_slug):
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        if request.method == 'GET':
+            registration_form = RegistrationForm()
+          
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                participant = registration_form.save()
+                selected_meetup.participants.add(participant)
+                selected_meetup.save()
+                return redirect('confirm_registration')
+                
         return render(request, 'meetups/meetup-details.html', {
             'meetup': selected_meetup,
-            'meetup_found': True
+            'meetup_found': True,
+            'form': registration_form
         })
+        
     except Exception as exc:
+        print(exc)
         return render(request, 'meetups/meetup-details.html', {
-            'meetup_found': False
+            'meetup_found': False,
         } )
+
+def confirm_registration(request):
+    return render(request, 'meetups/registration-success.html')
